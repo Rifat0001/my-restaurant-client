489e9c2256d61466c3298cf4017f2c85743097ce
Rifat0001 <mahmudrifat764@gmail.com>
2023-06-27 21:35:03 +0600

    feature

---
+++ src/components/pages/Featured/Featured.jsx
@@ -0,0 +1,27 @@
+import feature from '../../../assets/home/featured.jpg'
+import SectionHeading from '../../compo/SectionHeading/SectionHeading';
+import './Featured.css'
+const Featured = () => {
+    return (
+        <div className='feature mb-20'>
+            <div className='overlay py-10'>
+                <SectionHeading
+                    heading={'---Check it out---'}
+                    subheading={'FROM OUR MENU'}
+                ></SectionHeading>
+                <div className='flex md:mx-32 items-center justify-center gap-10 pb-10 mt-10 text-white'>
+                    <img className='w-[548px] h-[300px] rounded-md' src={feature} alt="" />
+                    <div>
+                        <p>March 20,2023</p>
+                        <p>Where can i get some?</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis veniam neque, error doloremque esse eligendi unde recusandae quas beatae, animi ab provident perspiciatis ipsum repellendus?</p>
+                        <button className='feature-btn'>Read More</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Featured;
--- src/components/pages/PopularMenu/PopularMenu.jsx
+++ src/components/pages/PopularMenu/PopularMenu.jsx
@@ -1,19 +1,32 @@
 import { useEffect, useState } from "react";
 import SectionHeading from "../../compo/SectionHeading/SectionHeading";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
     const [menu, setMenu] = useState([]);
     useEffect(() => {
         fetch('menu.json')
             .then(res => res.json())
-            .then(data => setMenu(data))
+            .then(data => {
+                const popularItems = data.filter(item => item.category === 'popular')
+                setMenu(popularItems)
+            })
     }, [])
     return (
-        <div>
+        <div className="mb-20">
             <SectionHeading
                 heading={"From Our Menu"}
                 subheading={"Popular Items"}
             ></SectionHeading>
+            <div className="grid my-10 md:grid-cols-2 gap-10">
+                {
+                    menu.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
+
         </div>
     );
 };
